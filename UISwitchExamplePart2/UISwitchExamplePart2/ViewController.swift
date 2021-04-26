f505c889c038e6d3265d9beea352a89067c42909
Aditi3 <aditi.gvc@gmail.com>
2021-04-26 16:13:50 +0530

    Added timer to perform UISwitch Animation

--- UISwitchExamplePart2/UISwitchExamplePart2/ViewController.swift
+++ UISwitchExamplePart2/UISwitchExamplePart2/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var index = 0.1
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func switchValueChanged (sender: UISwitch) {
+        let switchButtons:[UISwitch] = self.view.subviews.compactMap{ $0 as? UISwitch }
+        index = 0.1
+        for switchButton in switchButtons.sorted(by: { $0.tag < $1.tag })
+        {
+            Timer.scheduledTimer(withTimeInterval: index, repeats: false) { timer in
+                print("Timer fired! \(switchButton.tag)")
+                switchButton.setOn(sender.isOn ? true : false, animated: true)
+            }
+            self.index += 0.1
+        }
+    }
 }
 
